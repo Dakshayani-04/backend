e97f99165a20156cec1c97e05e7aa688a5a0eaad
Dakshayani-04 <23a91a1211@aec.edu.in>
2025-12-05 11:40:49 +0530

    added the get method file

---
+++ getmethod.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+app.get('/users',(req,res)=>{
+    res.send("hello this is from backend");
+})
+app.get('/get-users',(req,res)=>{
+    res.send("hello this is from backend");
+})
+
+app.listen(7007,()=>{
+    console.log("server running at port 7007")
+});
