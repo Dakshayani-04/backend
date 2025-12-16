d361e6495a4368c8b9cdce03dbe7beed45839c10
Dakshayani-04 <23a91a1211@aec.edu.in>
2025-12-16 11:52:47 +0530

    index changed

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import routers from './routers/studentRouters.js';
 const app = express();
-app.get('/',(req,res)=>{
-    res.send("hello this is from backend");
-})
+app.use(express.json())
+app.use('/',routers);
+//app.get('/',(req,res)=>{
+   // res.send("hello this is from backend");
+//})
 app.listen(7007,()=>{
     console.log("server running at port 7007")
 });
